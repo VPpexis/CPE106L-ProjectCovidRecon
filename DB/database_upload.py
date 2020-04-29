226eda860d4c8469b841c851f05208675c5024f6
Van Philip Panugan <panuganvanphilip@gmail.com>
2020-04-30 04:54:27 +0800

    Added Data to AWS Database through Py

---
+++ DB/database_upload.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import mysql.connector
+
+
+df = pd.read_csv('DB/COVID-19 Data 20204030.csv', encoding = "ISO-8859-1")
+is_NCR = df['RegionRes']=='NCR'
+
+cases_NCR = df[is_NCR]
+
+cases_NCR = cases_NCR.drop('RegionRes', 1)
+cases_NCR = cases_NCR.fillna('')
+
+
+#print(cases_NCR)
+
+mydb = mysql.connector.connect(
+    host = 'myrds1.cijcu6ghykxh.ap-southeast-1.rds.amazonaws.com',
+    user = 'myrds',
+    passwd = 'admin123',
+    database = 'myrds1'
+)
+
+mycursor = mydb.cursor()
+
+
+sql = 'INSERT INTO casesByNCR(case_code, age, age_group, sex, date_rep_conf, removal_type, admitted, city_mun_res, health_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);'
+cases_NCR = cases_NCR.values.tolist()
+mycursor.executemany(sql, cases_NCR)
+
+mydb.commit()
+
+
+
