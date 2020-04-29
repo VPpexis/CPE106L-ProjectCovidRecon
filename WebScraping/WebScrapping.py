83424da1a968c6fd7bd369e8e592e66e5af2a105
Van Philip Panugan <panuganvanphilip@gmail.com>
2020-04-29 13:40:36 +0800

    :cyclone: Updated Class to return data

--- WebScraping/WebScrapping.py
+++ WebScraping/WebScrapping.py
@@ -1,36 +1,48 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 
-#Allows an access to the website.
-with requests.Session() as se:
-    se.headers = {
-        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36 Edg/80.0.361.66',
-        'Accept-Encoding' : 'gzip, deflate',
-        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        'Accept-Language' : 'en'
-    }
-
-#Gets the URL of the website.
-result = se.get('https://www.inquirer.net/')
-
-#If it returns 200 then it is connected. Otherwise, it is not.
-#print(result.status_code)
-
-src = result.content
-soup = BeautifulSoup(src, 'lxml')
-
-#print(soup.find_all('a', limit=1))
+class WebScrapping():
+    def __init__(self):
+        self.articleName = []
+        self.articleLink = []
 
 
-urls = []
-for h2_tag in soup.find_all("h2"):
-    a_tag = h2_tag.find('a')
-    try:
-        if 'href' in a_tag.attrs:
-            urls.append(a_tag.attrs['href'])
-    except:
-        pass
+    def run(self):
+        #Allows an access to the website.
+        with requests.Session() as se:
+            se.headers = {
+                'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36 Edg/80.0.361.66',
+                'Accept-Encoding' : 'gzip, deflate',
+                'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                'Accept-Language' : 'en'
+            }
 
-for x in urls:
-    print(x)
+
+        #Gets the URL of the website.
+        result = se.get('https://www.doh.gov.ph/press-releases', verify=False)
+        src = result.content
+        soup = BeautifulSoup(src, 'lxml')
+
+
+        for content in soup.find_all('div', attrs = {'class':'region region-content'}):
+            for tags in content.find_all('span', attrs = {'class':'field-content'}):
+                for rows in tags.find_all('a'):
+                    self.articleName.append(rows.getText())
+                    self.articleLink.append('https://www.doh.gov.ph' + rows['href'])
+
+        d = {'Article Name': self.articleName, 'Link': self.articleLink}
+        self.df = pd.DataFrame(data=d)
+    
+    def getData(self):
+        return self.df.values.tolist()
+        
+
+if __name__ == '__main__':
+    ws = WebScrapping()
+    ws.run()
+    dataNews = ws.getData()
+
+    for x in dataNews:
+        print(x[1])
