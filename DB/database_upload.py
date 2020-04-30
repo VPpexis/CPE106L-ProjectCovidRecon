b9513c6b128d2a02650c628f21d74a79997776f1
Van Philip Panugan <panuganvanphilip@gmail.com>
2020-04-30 19:46:36 +0800

    Added PH Database to DB Server

--- DB/database_upload.py
+++ DB/database_upload.py
@@ -2,33 +2,59 @@ import pandas as pd
 import mysql.connector
 
 #Should be run on a server
+def ncrdb():
+    df = pd.read_csv('DB/COVID-19 Data 20204030.csv', encoding = "ISO-8859-1")
+    is_NCR = df['RegionRes']=='NCR'
 
-df = pd.read_csv('DB/COVID-19 Data 20204030.csv', encoding = "ISO-8859-1")
-is_NCR = df['RegionRes']=='NCR'
+    cases_NCR = df[is_NCR]
 
-cases_NCR = df[is_NCR]
-
-cases_NCR = cases_NCR.drop('RegionRes', 1)
-cases_NCR = cases_NCR.fillna('')
-
-
-#print(cases_NCR)
-
-mydb = mysql.connector.connect(
-    host = 'myrds1.cijcu6ghykxh.ap-southeast-1.rds.amazonaws.com',
-    user = 'myrds',
-    passwd = 'admin123',
-    database = 'myrds1'
-)
-
-mycursor = mydb.cursor()
+    cases_NCR = cases_NCR.drop('RegionRes', 1)
+    cases_NCR = cases_NCR.fillna('')
 
 
-sql = 'INSERT INTO casesByNCR(case_code, age, age_group, sex, date_rep_conf, removal_type, admitted, city_mun_res, health_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);'
-cases_NCR = cases_NCR.values.tolist()
-mycursor.executemany(sql, cases_NCR)
+    #print(cases_NCR)
 
-mydb.commit()
+    mydb = mysql.connector.connect(
+        host = 'myrds1.cijcu6ghykxh.ap-southeast-1.rds.amazonaws.com',
+        user = 'myrds',
+        passwd = 'admin123',
+        database = 'myrds1'
+    )
+
+    mycursor = mydb.cursor()
+
+
+    sql = 'INSERT INTO casesByNCR(case_code, age, age_group, sex, date_rep_conf, removal_type, admitted, city_mun_res, health_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);'
+    cases_NCR = cases_NCR.values.tolist()
+    mycursor.executemany(sql, cases_NCR)
+
+    mydb.commit()
+
+def ph_DB():
+    df = pd.read_csv('DB/COVID-19 Data 20204030.csv', encoding = "ISO-8859-1")
+
+    cases_ph = df.fillna('')
+
+    #print(cases_NCR)
+
+    mydb = mysql.connector.connect(
+        host = 'myrds1.cijcu6ghykxh.ap-southeast-1.rds.amazonaws.com',
+        user = 'myrds',
+        passwd = 'admin123',
+        database = 'myrds1'
+    )
+
+    mycursor = mydb.cursor()
+
+
+    sql = 'INSERT INTO casesByPH(case_code, age, age_group, sex, date_rep_conf, removal_type, admitted, region_res, city_mun_res, health_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
+    cases_ph = cases_ph.values.tolist()
+    mycursor.executemany(sql, cases_ph)
+
+    mydb.commit()
+
+ph_DB()
+
 
 
 
