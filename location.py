b9c2e33e652f2708cfe5613345cccf139b10efe2
Claudee Directo <claudeekdirecto@gmail.com>
2020-05-01 18:43:43 +0800

    updated choropleth with City labels 📌

--- location.py
+++ location.py
@@ -1,4 +1,14 @@
-import pandas as pd 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 17:32:22 2020
+
+@author: claudeedirecto
+"""
+
+import numpy as np
+import pandas as pd
+
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import mysql.connector
@@ -45,8 +55,6 @@ ncr_cities.replace('Malabon', 'City of Malabon', inplace=True)
 ncr_cities.replace('Las Piñas', 'City of Las Piñas', inplace=True)
 ncr_cities.replace('Kalookan City', 'Caloocan City', inplace=True)
 
-
-
 for index,rows in df.iterrows():
     if index not in ncr_cities['City'].to_list():
         print(items + ' is not in the NCR cities list')
@@ -56,12 +64,18 @@ for index,rows in df.iterrows():
 combined=ncr_cities.merge(df, on = 'City')
 ax =combined.plot(column = 'Cases_per_City',
         cmap = 'OrRd',
-        figsize = (10,10),
+        figsize = (22,22),
         legend = True,
         scheme = 'user_defined',
         classification_kwds = {'bins':[10, 20, 50, 100, 500, 1000, 5000, 10000, 500000]},
         edgecolor = 'black',
         linewidth = (0,0.8))
 ax.set_title('Total Confirmed COVID-19 Cases in NCR Today', fontdict =
-                  {'fontsize':20}, pad=12.5)
-ax.get_legend().set_bbox_to_anchor((1.3,0.3))
+                  {'fontsize':30}, pad=12.5)
+ax.get_legend().set_bbox_to_anchor((0.9,0.3))
+ax.annotate('Source: Wikipedia - https://en.wikipedia.org/wiki/Metro_Manila', xy=(0.6, .05), xycoords='figure fraction', fontsize=12, color='#FFFFFF')
+combined['coords'] = combined['geometry'].apply(lambda x: x.representative_point().coords[:])
+combined['coords'] = [coords[0] for coords in combined['coords']]
+for idx, row in combined.iterrows():
+    plt.annotate(s=row['City'], xy=row['coords'],horizontalalignment='center')
+    
