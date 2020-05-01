44855df41fb676996c42f37c3fb4a05e9ea13006
Van Philip Panugan <panuganvanphilip@gmail.com>
2020-05-01 09:36:37 +0800

    Updated the AWS RDS

--- DB/database_upload.py
+++ DB/database_upload.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import mysql.connector
 
-#Should be run on a server
-def ncrdb():
-    df = pd.read_csv('DB/COVID-19 Data 20204030.csv', encoding = "ISO-8859-1")
+#Should be run on a AWS EC2 Instance.
+#Link of DOH Covid-19 Data: https://drive.google.com/drive/folders/1w_O-vweBFbqCgzgmCpux2F0HVB4P6ni2
+#Need to be a streamlistener using GDrive API to automatically get data in GDrive.
+def ncrdb(databaseloc):
+    df = pd.read_csv(databaseloc, encoding = "ISO-8859-1")
     is_NCR = df['RegionRes']=='NCR'
 
     cases_NCR = df[is_NCR]
@@ -24,18 +26,18 @@ def ncrdb():
     mycursor = mydb.cursor()
 
 
-    sql = 'INSERT INTO casesByNCR(case_code, age, age_group, sex, date_rep_conf, removal_type, admitted, city_mun_res, health_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);'
+    sql = 'INSERT IGNORE INTO casesByNCR(case_code, age, age_group, sex, date_rep_conf, removal_type, admitted, city_mun_res, health_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);'
     cases_NCR = cases_NCR.values.tolist()
     mycursor.executemany(sql, cases_NCR)
 
     mydb.commit()
 
-def ph_DB():
-    df = pd.read_csv('DB/COVID-19 Data 20204030.csv', encoding = "ISO-8859-1")
+def ph_DB(databaseloc):
+    df = pd.read_csv(databaseloc, encoding = "ISO-8859-1")
 
     cases_ph = df.fillna('')
 
-    #print(cases_NCR)
+    print(cases_ph)
 
     mydb = mysql.connector.connect(
         host = 'myrds1.cijcu6ghykxh.ap-southeast-1.rds.amazonaws.com',
@@ -47,13 +49,16 @@ def ph_DB():
     mycursor = mydb.cursor()
 
 
-    sql = 'INSERT INTO casesByPH(case_code, age, age_group, sex, date_rep_conf, removal_type, admitted, region_res, city_mun_res, health_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
+    sql = 'INSERT IGNORE INTO casesByPH(case_code, age, age_group, sex, date_rep_conf, removal_type, admitted, region_res, city_mun_res, health_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
     cases_ph = cases_ph.values.tolist()
+    print(cases_ph)
     mycursor.executemany(sql, cases_ph)
 
     mydb.commit()
+    mydb.close()
 
-ph_DB()
+ph_DB('DB/DOH Data Drop 20200430.csv')
+ncrdb('DB/DOH Data Drop 20200430.csv')
 
 
 
